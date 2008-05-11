7f374090b40b5b5d5ffc44bc0570215013277dbb
vlimant <>
2008-05-11 21:24:40 +0000

    improved version with TFileService and edm

--- cmssw/WorkSpace/ConfigurableAnalysis/interface/NTupler.h
+++ cmssw/WorkSpace/ConfigurableAnalysis/interface/NTupler.h
@@ -8,7 +8,11 @@
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/EDFilter.h"
+#include <FWCore/Framework/interface/ProducerBase.h>
 
+#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+#include "TTree.h"
+#include "TBranch.h"
 
 /*
  * Description:
@@ -18,11 +22,15 @@
 
 class NTupler {
  public:
-  NTupler(){}
+  NTupler() : useTFileService_(false){}
   virtual ~NTupler(){}
 
-  virtual uint registerleaves(edm::EDFilter * producer) =0;
+  //  virtual uint registerleaves(edm::EDFilter * producer) =0;
+  virtual uint registerleaves(edm::ProducerBase * producer) =0;
   virtual void fill(edm::Event& iEvent)=0;
+ protected:
+  bool useTFileService_;
+  TTree * tree_;
 };
 
 
@@ -45,12 +53,17 @@ class TreeBranch {
   typedef std::auto_ptr<std::vector<double> > value;
   value branch(const edm::Event& iEvent);
 
+  std::vector<double>** dataHolderPtrAdress() { return &dataHolderPtr_;}
+  std::vector<double>* dataHolderPtr() { return dataHolderPtr_;}
+  void assignDataHolderPtr(std::vector<double> * data) { dataHolderPtr_=data;}
  private:
   std::string class_;
   edm::InputTag src_;
   std::string expr_;
   std::string maxIndexName_;
   std::string branchAlias_;
+
+  std::vector<double> * dataHolderPtr_;
 };
 
 template <typename Object, typename Collection=std::vector<Object> >
@@ -77,9 +90,10 @@ public:
 	//actually fill the vector of values
 	uint i_end=oH->size();
 	for (uint i=0;i!=i_end;++i){
-	  //	  try {
+	  try {
 	    (*value_)[i]=(expr)((*oH)[i]); 
-	    //	  }catch(...){	    edm::LogError("StringBranchHelper")<<"could not evaluate expression: "<<B.expr()<<" on class: "<<B.className(); }
+	  }catch(...){
+	    edm::LogError("StringBranchHelper")<<"could not evaluate expression: "<<B.expr()<<" on class: "<<B.className(); }
 	}
       }
     }
@@ -90,7 +104,8 @@ public:
 class VariableNTupler : public NTupler{
  public:
   VariableNTupler(const edm::ParameterSet& iConfig){
-    if (iConfig.getParameter<bool>("allVariables"))
+    edm::ParameterSet variablePSet=iConfig.getParameter<edm::ParameterSet>("variablesPSet");
+    if (variablePSet.getParameter<bool>("allVariables"))
       {
 	VariableHelper::iterator v=VariableHelperInstance::get().begin();
 	VariableHelper::iterator v_end=VariableHelperInstance::get().end();
@@ -99,40 +114,74 @@ class VariableNTupler : public NTupler{
 	}
       }
     else{
-      std::vector<std::string> leaves=iConfig.getParameter<std::vector<std::string> >("leaves");
+      std::vector<std::string> leaves=variablePSet.getParameter<std::vector<std::string> >("leaves");
       for (uint i=0;i!=leaves.size();++i){
 	leaves_[leaves[i]]= VariableHelperInstance::get().variable(leaves[i]);
       }
     }
+    if (variablePSet.exists("useTFileService"))
+      useTFileService_=variablePSet.getParameter<bool>("useTFileService");
+    else
+      useTFileService_=iConfig.getParameter<bool>("useTFileService");
   }
   
-  uint registerleaves(edm::EDFilter * producer){
-    //loop the leaves registered
+  //  uint registerleaves(edm::EDFilter * producer){
+  uint registerleaves(edm::ProducerBase * producer){
     uint nLeaves=0;
-    iterator i=leaves_.begin();
-    iterator i_end= leaves_.end();
-    for(;i!=i_end;++i){
-      nLeaves++;
-      producer->produces<double>(i->first).setBranchAlias(i->first);
+    if (useTFileService_){
+      //loop the leaves registered
+      nLeaves=leaves_.size();
+      // make arrays of pointer to the actual values
+      dataHolder_=new double[nLeaves];
+      iterator i=leaves_.begin();
+      iterator i_end= leaves_.end();
+      edm::Service<TFileService> fs;
+      tree_=fs->make<TTree>("variable","VariableNTuple tree");
+      uint iInDataHolder=0;
+      for(;i!=i_end;++i,++iInDataHolder){
+	tree_->Branch(i->first.c_str(), &(dataHolder_[iInDataHolder]), (i->first+"/D").c_str());
+      }
+    }else{
+      //loop the leaves registered
+      iterator i=leaves_.begin();
+      iterator i_end= leaves_.end();
+      for(;i!=i_end;++i){
+	nLeaves++;
+	producer->produces<double>(i->first).setBranchAlias(i->first);
+      }
     }
     return nLeaves;
   }
-
+  
   void fill(edm::Event& iEvent){
     //protection against stupid users :-(
     if (!edm::Service<UpdaterService>()->checkOnce("VariableNTupler::fill")) return;
     
-    //other leaves
-    iterator i=leaves_.begin();
-    iterator i_end=leaves_.end();
-    for(;i!=i_end;++i){
-      std::auto_ptr<double> leafValue(new double((*i->second)()));
-      iEvent.put(leafValue, i->first);
+    if (useTFileService_){
+      //fill the data holder
+      iterator i=leaves_.begin();
+      iterator i_end=leaves_.end();
+      uint iInDataHolder=0;
+      for(;i!=i_end;++i,++iInDataHolder){
+	dataHolder_[iInDataHolder]=(*i->second)();
+      }
+      //fill into root;
+      tree_->Fill();
+    }else{
+      //other leaves
+      iterator i=leaves_.begin();
+      iterator i_end=leaves_.end();
+      for(;i!=i_end;++i){
+	std::auto_ptr<double> leafValue(new double((*i->second)()));
+	iEvent.put(leafValue, i->first);
+      }
     }
   }
  protected:
   typedef std::map<std::string, const CachingVariable *>::iterator iterator;
   std::map<std::string, const CachingVariable *> leaves_;
+
+  double * dataHolder_;
 };
 
 
@@ -155,24 +204,57 @@ class StringBasedNTupler : public NTupler {
 	branches_[maxName].push_back(TreeBranch(className, src, leave_expr, maxName, branchAlias));
       }//loop the provided leaves
     }//loop the provided branches
-
+    if (branchesPSet.exists("useTFileService"))
+      useTFileService_=branchesPSet.getParameter<bool>("useTFileService");         
+    else
+      useTFileService_=iConfig.getParameter<bool>("useTFileService");
+    treeName_=branchesPSet.getParameter<std::string>("treeName");
   }
 
-  uint registerleaves(edm::EDFilter * producer){
+  //  uint registerleaves(edm::EDFilter * producer){
+  uint registerleaves(edm::ProducerBase * producer){
     uint nLeaves=0;
-    // loop the automated leafer
-    Branches::iterator iB=branches_.begin();
-    Branches::iterator iB_end=branches_.end();
-    for(;iB!=iB_end;++iB){
-      //the index. should produce it only once
-      nLeaves++;
-      producer->produces<uint>(iB->first).setBranchAlias(iB->first);
-      std::vector<TreeBranch>::iterator iL=iB->second.begin();
-      std::vector<TreeBranch>::iterator iL_end=iB->second.end();
-      for(;iL!=iL_end;++iL){
-	TreeBranch & b=*iL;
-	//the values
-	producer->produces<std::vector<double> >(b.branchName()).setBranchAlias(b.branchAlias());
+
+    if (useTFileService_){
+      edm::Service<TFileService> fs;
+      tree_=fs->make<TTree>(treeName_.c_str(),"StringBasedNTupler tree");
+      //reserve memory for the indexes      
+      indexDataHolder_ = new uint[branches_.size()];
+      // loop the automated leafer
+      Branches::iterator iB=branches_.begin();
+      Branches::iterator iB_end=branches_.end();
+      uint indexOfIndexInDataHolder=0;
+      for(;iB!=iB_end;++iB,++indexOfIndexInDataHolder){
+	//branch for the index
+	//---	std::cout<<"happy so far 1"<<std::endl;
+	tree_->Branch(iB->first.c_str(), &(indexDataHolder_[indexOfIndexInDataHolder]),(iB->first+"/i").c_str());
+	//---	std::cout<<"happy so far 2"<<std::endl;
+	std::vector<TreeBranch>::iterator iL=iB->second.begin();
+	std::vector<TreeBranch>::iterator iL_end=iB->second.end();
+	for(;iL!=iL_end;++iL){
+	  TreeBranch & b=*iL;
+	  //---	  std::cout<<"happy so far 3"<<std::endl;
+	  //branch for the leave
+	  tree_->Branch(b.branchAlias().c_str(),"std::vector<double>",iL->dataHolderPtrAdress());
+	  //---	  std::cout<<"happy so far 4"<<std::endl;
+	}
+      }
+    }
+    else{
+      // loop the automated leafer
+      Branches::iterator iB=branches_.begin();
+      Branches::iterator iB_end=branches_.end();
+      for(;iB!=iB_end;++iB){
+	//the index. should produce it only once
+	nLeaves++;
+	producer->produces<uint>(iB->first).setBranchAlias(iB->first);
+	std::vector<TreeBranch>::iterator iL=iB->second.begin();
+	std::vector<TreeBranch>::iterator iL_end=iB->second.end();
+	for(;iL!=iL_end;++iL){
+	  TreeBranch & b=*iL;
+	  //the values
+	  producer->produces<std::vector<double> >(b.branchName()).setBranchAlias(b.branchAlias());
+	}
       }
     }
     return nLeaves;
@@ -180,32 +262,68 @@ class StringBasedNTupler : public NTupler {
 
   void fill(edm::Event& iEvent){
     //protection against stupid users :-(
-    if (!edm::Service<UpdaterService>()->checkOnce("StringBasedNTupler::fill")) return;
+    //    if (!edm::Service<UpdaterService>()->checkOnce("StringBasedNTupler::fill")) return;
+    //well if you do that, you cannot have two ntupler of the same type in the same job...
 
-    // loop the automated leafer
-    Branches::iterator iB=branches_.begin();
-    Branches::iterator iB_end=branches_.end();
-    for(;iB!=iB_end;++iB){
-      std::vector<TreeBranch>::iterator iL=iB->second.begin();
-      std::vector<TreeBranch>::iterator iL_end=iB->second.end();
-      uint maxS=0;
-      for(;iL!=iL_end;++iL){
-	TreeBranch & b=*iL;
-	std::auto_ptr<std::vector<double> > branch(b.branch(iEvent));
-	if (branch->size()>maxS) maxS=branch->size();
-	iEvent.put(branch, b.branchName());
+    if (useTFileService_){
+      // loop the automated leafer
+      Branches::iterator iB=branches_.begin();
+      Branches::iterator iB_end=branches_.end();
+      uint indexOfIndexInDataHolder=0;
+      for(;iB!=iB_end;++iB,++indexOfIndexInDataHolder){
+	std::vector<TreeBranch>::iterator iL=iB->second.begin();
+	std::vector<TreeBranch>::iterator iL_end=iB->second.end();
+	uint maxS=0;
+	for(;iL!=iL_end;++iL){
+	  TreeBranch & b=*iL;
+	  //---	  std::cout<<"happy so far 5"<<std::endl;
+	  std::auto_ptr<std::vector<double> > branch(b.branch(iEvent));
+	  //---	  std::cout<<"happy so far 6"<<std::endl;
+	  if (branch->size()>maxS) maxS=branch->size();
+	  //---	  std::cout<<"happy so far 7"<<std::endl;
+	  b.assignDataHolderPtr(branch.release());
+	  //---	  std::cout<<"happy so far 8"<<std::endl;
+	}
+	indexDataHolder_[indexOfIndexInDataHolder]=maxS;
       }
-      //index should be put only once per branch
-      //FIXME. max or min of branch size?
-      std::auto_ptr<uint> maxN(new uint(maxS));
-      iEvent.put(maxN, iB->first);
+      tree_->Fill();
+
+      //de-allocate memory now: allocated in branch(...) and released to the pointer.
+      for(;iB!=iB_end;++iB,++indexOfIndexInDataHolder){
+	std::vector<TreeBranch>::iterator iL=iB->second.begin();
+	std::vector<TreeBranch>::iterator iL_end=iB->second.end();
+	for(;iL!=iL_end;++iL){
+          TreeBranch & b=*iL;
+	  delete b.dataHolderPtr();
+	}}
+    }else{
+      // loop the automated leafer
+      Branches::iterator iB=branches_.begin();
+      Branches::iterator iB_end=branches_.end();
+      for(;iB!=iB_end;++iB){
+	std::vector<TreeBranch>::iterator iL=iB->second.begin();
+	std::vector<TreeBranch>::iterator iL_end=iB->second.end();
+	uint maxS=0;
+	for(;iL!=iL_end;++iL){
+	  TreeBranch & b=*iL;
+	  std::auto_ptr<std::vector<double> > branch(b.branch(iEvent));
+	  if (branch->size()>maxS) maxS=branch->size();
+	  iEvent.put(branch, b.branchName());
+	}
+	//index should be put only once per branch
+	//FIXME. max or min of branch size?
+	std::auto_ptr<uint> maxN(new uint(maxS));
+	iEvent.put(maxN, iB->first);
+    }
     }
   }
+
   protected:
   typedef std::map<std::string, std::vector<TreeBranch> > Branches;
   Branches branches_;
 
-
+  std::string treeName_;
+  uint * indexDataHolder_;
 };
 
 
@@ -216,7 +334,9 @@ class CombinedNTupler : public NTupler{
     all_.push_back(new VariableNTupler(iConfig));
     all_.push_back(new StringBasedNTupler(iConfig));
   }
-  uint registerleaves(edm::EDFilter * producer){
+
+  //  uint registerleaves(edm::EDFilter * producer){
+  uint registerleaves(edm::ProducerBase * producer){
     uint nLeaves=0;
     for (uint n=0;n!=all_.size();++n) nLeaves+=all_[n]->registerleaves(producer);
     return nLeaves;}
---
+++ cmssw/WorkSpace/ConfigurableAnalysis/plugins/NtuplerEDProducer.cc
@@ -0,0 +1,95 @@
+// -*- C++ -*-
+//
+// Package:    NtuplerEDProducer
+// Class:      NtuplerEDProducer
+// 
+/**\class NtuplerEDProducer NtuplerEDProducer.cc Workspace/NtuplerEDProducer/src/NtuplerEDProducer.cc
+
+ Description: <one line class summary>
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Jean-Roch Vlimant
+//         Created:  Sun May 11 21:12:46 CEST 2008
+// $Id$
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "Workspace/ConfigurableAnalysis/interface/NTupler.h"
+
+//
+// class decleration
+//
+
+class NtuplerEDProducer : public edm::EDProducer {
+   public:
+      explicit NtuplerEDProducer(const edm::ParameterSet&);
+      ~NtuplerEDProducer();
+
+   private:
+      virtual void beginJob(const edm::EventSetup&) ;
+      virtual void produce(edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+      
+      // ----------member data ---------------------------
+  NTupler * ntupler_;
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+NtuplerEDProducer::NtuplerEDProducer(const edm::ParameterSet& iConfig)
+{
+  ntupler_ = new StringBasedNTupler(iConfig.getParameter<edm::ParameterSet>("Ntupler"));
+  ntupler_->registerleaves(this);
+  produces<double>("dummy");
+}
+
+
+NtuplerEDProducer::~NtuplerEDProducer(){}
+
+// ------------ method called to produce the data  ------------
+void
+NtuplerEDProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  ntupler_->fill(iEvent);
+  std::auto_ptr<double> v(new double);
+  iEvent.put(v,"dummy");
+}
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+NtuplerEDProducer::beginJob(const edm::EventSetup&)
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+NtuplerEDProducer::endJob() {
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(NtuplerEDProducer);
