cc5edec1e7ef795cd82c01afb5d833625968b5bb
rebassoo <>
2009-03-23 17:43:59 +0000

    Added lines to create tcMET collection

--- cmssw/WorkSpace/ConfigurableAnalysis/python/testFromAOD_cfg.py
+++ cmssw/WorkSpace/ConfigurableAnalysis/python/testFromAOD_cfg.py
@@ -35,6 +35,14 @@ process.GlobalTag.globaltag = 'IDEAL_V11::All'
 process.load('Configuration/StandardSequences/MagneticField_38T_cff')
 #process.load("Configuration.StandardSequences.MagneticField_cff")
 
+#--------------------------------------------------------------------
+#needed for tcMET
+#--------------------------------------------------------------------
+# tracking geometry
+process.load("Geometry.CommonDetUnit.globalTrackingGeometry_cfi")
+#det id
+process.load("TrackingTools.TrackAssociator.DetIdAssociatorESProducer_cff")
+
 # PAT Layer 0+1
 process.load("PhysicsTools.PatAlgos.patLayer0_cff")
 process.load("PhysicsTools.PatAlgos.patLayer1_cff")
@@ -43,6 +51,17 @@ process.load("PhysicsTools.PatAlgos.patLayer1_cff")
 process.load("Workspace.ConfigurableAnalysis.configurableAnalysis_cff")
 process.load("SusyAnalysis.PatCrossCleaner.patCrossCleaner_cfi")
 
+#-------------------------------------------------
+#load met muon corrections
+#-------------------------------------------------
+
+process.load("JetMETCorrections.Type1MET.MetMuonCorrections_cff")
+
+#-------------------------------------------------
+# load tcMET producer
+#-------------------------------------------------
+
+process.load("RecoMET.METProducers.TCMET_cfi")
 
 ## Necessary fixes to run 2.2.X on 2.1.X data
 from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run22XonSummer08AODSIM
@@ -55,6 +74,7 @@ process.CaloTowerConstituentsMapBuilder = cms.ESProducer("CaloTowerConstituentsM
                                                          )
 
 process.p = cms.Path(
+                process.MetMuonCorrections*process.tcMet+
                 process.patLayer0  
                 #+ process.content # uncomment to get a dump of the output after layer 0
                 + process.patLayer1
