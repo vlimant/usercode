0d9108da801b2165a9ab6ffc803d43290dd39db2
vlimant <>
2010-03-18 12:45:35 +0000

    adhov is optional

--- cmssw/WorkSpace/ConfigurableAnalysis/interface/CompleteNTupler.h
+++ cmssw/WorkSpace/ConfigurableAnalysis/interface/CompleteNTupler.h
@@ -9,23 +9,32 @@ class CompleteNTupler : public NTupler {
   CompleteNTupler(const edm::ParameterSet& iConfig){
     sN = new StringBasedNTupler(iConfig);
     vN = new VariableNTupler(iConfig);
+    if (iConfig.exists("AdHocNPSet"))
+      if (!iConfig.getParameter<edm::ParameterSet>("AdHocNPSet").empty())
+	aN = new AdHocNTupler(iConfig);
+      else aN=0;
+    else
+      aN=0;
   }
   
   uint registerleaves(edm::ProducerBase * producer){
     uint nLeaves=0;
     nLeaves+=sN->registerleaves(producer);
     nLeaves+=vN->registerleaves(producer);
-    nLeaves+=aN->registerleaves(producer);
+    if (aN)
+      nLeaves+=aN->registerleaves(producer);
     return nLeaves;
   }
   void fill(edm::Event& iEvent){
     sN->fill(iEvent);
     vN->fill(iEvent);
-    aN->fill(iEvent);
+    if (aN)
+      aN->fill(iEvent);
 
     sN->callBack();
     vN->callBack();
-    aN->callBack();
+    if (aN)
+      aN->callBack();
   }
 
  private:
