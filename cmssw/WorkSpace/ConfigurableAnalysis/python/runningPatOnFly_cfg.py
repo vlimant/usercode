b3ed05ca6a67df7a3ec1444ff4b03087e828cf96
rebassoo <>
2010-03-30 22:04:14 +0000

    made changes for 3_5_x

--- cmssw/WorkSpace/ConfigurableAnalysis/python/runningPatOnFly_cfg.py
+++ cmssw/WorkSpace/ConfigurableAnalysis/python/runningPatOnFly_cfg.py
@@ -1,9 +1,9 @@
 #
 #  SUSY-PAT configuration file
 #
-#  PAT configuration for the SUSY group - 33X series
+#  PAT configuration for the SUSY group - 35X series
 #  More information here:
-#  https://twiki.cern.ch/twiki/bin/view/CMS/SusyPatLayer1DefV7
+#  https://twiki.cern.ch/twiki/bin/view/CMS/SusyPatLayer1DefV8
 #
 
 # Starting with a skeleton process which gets imported with the following line
@@ -21,9 +21,9 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 ## Source
 process.source = cms.Source("PoolSource",
-                                fileNames = cms.untracked.vstring(
-            '/store/relval/CMSSW_3_3_0/RelValTTbar/GEN-SIM-RECO/STARTUP31X_V8-v1/0001/3291E09D-67B7-DE11-9ED6-003048678C9A.root'
-                )
+                            fileNames = cms.untracked.vstring(
+    '/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0006/14920B0A-0DE8-DE11-B138-002618943926.root'
+    )
                             )
 ## Maximal Number of Events
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
@@ -31,49 +31,29 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 ## Geometry and Detector Conditions (needed for a few patTuple production steps)
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-## global tag for 33X
-process.GlobalTag.globaltag = cms.string('STARTUP31X_V1::All')
-## global tag for 34X
-## process.GlobalTag.globaltag = cms.string('STARTUP3XY_V9::All')
+#process.GlobalTag.globaltag = cms.string('MC_3XY_V20::All')
+process.GlobalTag.globaltag = cms.string('START3X_V20::All')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
 ## Standard PAT Configuration File
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
 
-from PhysicsTools.PatAlgos.tools.jetTools import *
-print "*********************************************************************"
-print "Switching all processes to use the anti-kT algorithm by default."
-print "Switch the jet collection to your desired algorithm if this is not"
-print "what you want to use. Note that L7Parton correction are taken from"
-print "SC5 instead of AK5. This is an intermediate solution for the time "
-print "being."
-print "*********************************************************************"
-switchJetCollection(process,
-                                        cms.InputTag('ak5CaloJets'),
-                                        doJTA            = True,
-                                        doBTagging       = True,
-                                        jetCorrLabel     = ('AK5','Calo'),
-                                        doType1MET       = True,
-                                        genJetCollection = cms.InputTag("ak5GenJets")
-                                        )
-
-# Output Module Configuration (expects a path 'p')
+## Output Module Configuration (expects a path 'p')
 from PhysicsTools.PatAlgos.patEventContent_cff import patEventContent
 process.out = cms.OutputModule("PoolOutputModule",
-                                                              fileName = cms.untracked.string('PATLayer1_Output.fromAOD_full.root'),
-                                                              # save only events passing the full path
-                                                              SelectEvents   = cms.untracked.PSet( SelectEvents = cms.vstring('p') ),
-                                                              # save PAT Layer 1 output; you need a '*' to
-                                                              # unpack the list of commands 'patEventContent'
-                                                              outputCommands = cms.untracked.vstring('drop *', *patEventContent )
-                                                              )
+                               fileName = cms.untracked.string('PATLayer1_Output.fromAOD_full.root'),
+                               # save only events passing the full path
+                               SelectEvents   = cms.untracked.PSet( SelectEvents = cms.vstring('p') ),
+                               # save PAT Layer 1 output; you need a '*' to
+                               # unpack the list of commands 'patEventContent'
+                               outputCommands = cms.untracked.vstring('drop *', *patEventContent )
+                               )
 #process.outpath = cms.EndPath(process.out)
-
 
 
 #-- Meta data to be logged in DBS ---------------------------------------------
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.21 $'),
+    version = cms.untracked.string('$Revision: 1.26 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/PhysicsTools/Configuration/test/SUSY_pattuple_cfg.py,v $'),
     annotation = cms.untracked.string('SUSY pattuple definition')
 )
@@ -88,8 +68,8 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 #-- Input Source --------------------------------------------------------------
 process.source.fileNames = [
-     'rfio://?svcclass=cmscafuser&path=/castor/cern.ch/user/n/nmohr/QCDDiJet_Pt380to470_MC_31X_V9_ReReco332.root'
-#     '/store/data/BeamCommissioning09/MinimumBias/RECO/rereco_FIRSTCOLL_v1/0083/FE5EDBBC-7DD9-DE11-9589-001A92971B64.root'
+     '/store/relval/CMSSW_3_5_2/RelValTTbar/GEN-SIM-RECO/MC_3XY_V21-v1/0016/F257D874-3C1E-DF11-AF61-001731A28BE1.root'
+     #'/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/18thFebPreProd_351p1_RecoTracks-v2/0000/FC52ED81-511E-DF11-8743-00237DA13FB6.root'
     ]
 process.maxEvents.input = 10
 # Due to problem in production of LM samples: same event number appears multiple times
@@ -97,22 +77,14 @@ process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 
 #-- Calibration tag -----------------------------------------------------------
 # Should match input file's tag
-process.GlobalTag.globaltag = 'GR09_P_V6::All' #GR09_P_V6 for data, MC_31X_V9 for MC
-
-#-- Missing ak5GenJets in 3.3.2 samples ---------------------------------------
-# Comment this when you run on data
-#from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run33xOnReRecoMC
-#run33xOnReRecoMC( process, "ak5GenJets" )
+process.GlobalTag.globaltag = 'MC_3XY_V24::All'
 
 ############################# START SUSYPAT specifics ####################################
-from PhysicsTools.Configuration.SUSY_pattuple_cff import addDefaultSUSYPAT, removeMCDependence, getSUSY_pattuple_outputCommands
-# Uncomment next line when you run on data
-removeMCDependence(process)
-#addDefaultSUSYPAT(process,'HLT8E29') #second parameter is the name of the HLT menu
-addDefaultSUSYPAT(process,'HLT8E29','900GeV') #second parameter is the name of the HLT menu
+from PhysicsTools.Configuration.SUSY_pattuple_cff import addDefaultSUSYPAT, getSUSY_pattuple_outputCommands
+#Apply SUSYPAT, parameters are: mcInfo, HLT menu, Jet energy corrections, JetCollections
+addDefaultSUSYPAT(process,True,'HLT','Summer09_7TeV_ReReco332',['IC5Calo','SC5Calo','AK5PF','AK5JPT','AK5Track']) 
 SUSY_pattuple_outputCommands = getSUSY_pattuple_outputCommands( process )
 ############################## END SUSYPAT specifics ####################################
-
 
 #-- configurableAnalysis stuff -------------------------------------------------------
 process.load("Workspace.ConfigurableAnalysis.configurableAnalysis_ForPattuple_cff")
@@ -128,5 +100,8 @@ process.out.outputCommands = cms.untracked.vstring('drop *', *SUSY_pattuple_outp
 
 #-- Execution path ------------------------------------------------------------
 # Full path
-process.p = cms.Path( process.seqSUSYDefaultSequence +process.configurableAnalysis)
-
+process.p = cms.Path( process.seqSUSYDefaultSequence + process.configurableAnalysis )
+#-- Dump config ------------------------------------------------------------
+file = open('SusyPAT_cfg.py','w')
+file.write(str(process.dumpPython()))
+file.close()
