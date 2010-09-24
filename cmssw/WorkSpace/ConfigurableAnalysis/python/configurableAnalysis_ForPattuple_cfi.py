b7d668884841e4760efc1e8d88b2c63e27661994
wto <>
2010-09-24 18:04:15 +0000

    Put the Ecal Endcap Clusters back

--- cmssw/WorkSpace/ConfigurableAnalysis/python/configurableAnalysis_ForPattuple_cfi.py
+++ cmssw/WorkSpace/ConfigurableAnalysis/python/configurableAnalysis_ForPattuple_cfi.py
@@ -1001,7 +1001,22 @@ configurableAnalysis = cms.EDFilter("ConfigurableAnalysis",
             	),
             	Class = cms.string('reco::CaloCluster')
           	),
-						
+						multi5x5EBC = cms.PSet( 	 
+	          	src = cms.InputTag("multi5x5BasicClusters","multi5x5EndcapBasicClusters"),    
+	          	leaves = cms.PSet(    
+	          		vars = cms.vstring(   
+	          	  	'energy:energy',    
+	          	  	'x:x',    
+	          	  	'y:y',    
+	          	  	'z:z',    
+	          	  	'rho:position.rho',   
+	          	  	'phi:phi',    
+	          	  	'eta:eta',    
+	          	  	'theta:position.theta'    
+	          	 	)   
+	          	),    
+	          	Class = cms.string('reco::CaloCluster')   
+	          ),
 						convTk = cms.PSet(
               src = cms.InputTag("trackerOnlyConversions",""),
               leaves = cms.PSet(
--- cmssw/WorkSpace/ConfigurableAnalysis/python/nTuplerTemplate_cfg.py
+++ cmssw/WorkSpace/ConfigurableAnalysis/python/nTuplerTemplate_cfg.py
@@ -36,14 +36,21 @@ process.out.outputCommands = cms.untracked.vstring('drop *', *SUSY_pattuple_outp
 # Change input filenames and setup options for data/MC.
 process.source = cms.Source("PoolSource",
 	fileNames = cms.untracked.vstring(
-	'file:/DataE/wto/Reco/Commissioning10-SD_JetMETTau-Jun9thSkim_v1RECO.root')
+	#'file:/DataE/wto/Reco/Commissioning10-SD_JetMETTau-Jun9thSkim_v1RECO.root')
+		'file:/DataE/wto/Reco/Mu_Run2010B-PromptReco-v2_RECO/BCA934AE-FAC6-DF11-9143-003048F024FA.root',
+		'file:/DataE/wto/Reco/Mu_Run2010B-PromptReco-v2_RECO/289F629E-F3C6-DF11-BB6E-001D09F231B0.root',
+		'file:/DataE/wto/Reco/Mu_Run2010B-PromptReco-v2_RECO/4030584E-D7C6-DF11-BDB0-000423D9890C.root',
+		'file:/DataE/wto/Reco/Mu_Run2010B-PromptReco-v2_RECO/42BD67BF-F4C6-DF11-9704-0030487C608C.root',
+		'file:/DataE/wto/Reco/Mu_Run2010B-PromptReco-v2_RECO/60BD2F82-DDC6-DF11-AA84-001D09F2910A.root',
+		'file:/DataE/wto/Reco/Mu_Run2010B-PromptReco-v2_RECO/A660F145-F6C6-DF11-A67F-001D09F2516D.root',
+	)
 )
 
 #process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck') #check for dupes.
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 #nTuple output file
-process.TFileService.fileName= 'nTuple2.root' #nTuple filename.
+process.TFileService.fileName= 'nTuple.root' #nTuple filename.
 
 #Apply SUSYPAT, parameters are: mcInfo, HLT menu, Jet energy corrections, JetCollections
 
@@ -69,7 +76,8 @@ process.TFileService.fileName= 'nTuple2.root' #nTuple filename.
 addDefaultSUSYPAT(process,False,'HLT','Spring10','',['IC5Calo','AK5JPT']) 
 #process.GlobalTag.globaltag = 'GR_R_37X_V6A::All'
 #needed to run in 38X... seems to work on 37X RECO also...
-process.GlobalTag.globaltag = cms.string('START38_V8::All') 
+#process.GlobalTag.globaltag = cms.string('START38_V8::All') 
+process.GlobalTag.globaltag = cms.string('GR10_P_V10::All')
 
 ### Setting for Running on 37X MC 
 #addDefaultSUSYPAT(process,True,'HLT','Spring10','',['IC5Calo','AK5JPT']) 
